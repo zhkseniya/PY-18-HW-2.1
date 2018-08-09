3d5e2fd055b846987f7b06aee5b3c4d64f4c86d8
Kseniya Zhidkikh <39521654+zhkseniya@users.noreply.github.com>
2018-08-10 00:20:21 +0600

    Add files via upload

--- hw_2_1.py
+++ hw_2_1.py
@@ -5,12 +5,12 @@ def get_cookbook():
     READ_INGREDIENT = 3
 
     cookbook = {}
-    current_state = 1
+    current_state = READ_EMPTY_LINE
     
     with open('cookbook.txt') as cookbook_file:
         for cookbook_line in cookbook_file:
             cookbook_line_without_space = cookbook_line.strip()
-            if cookbook_line_without_space == "":
+            if not cookbook_line_without_space:
                 current_state = READ_EMPTY_LINE
                 continue
             if current_state == READ_EMPTY_LINE:
@@ -23,28 +23,26 @@ def get_cookbook():
                 if dish not in cookbook:
                     cookbook[dish] = list()
                 cookbook[dish].append({
-                    'ingridient_name': list_ingredients[0].strip(), 
+                    'ingridient_name': list_ingredients[0].strip(),
                     'quantity': int(list_ingredients[1].strip()), 
                     'measure': list_ingredients[2].strip()
                 })
         return cookbook
 
 # Задание 2
-def get_shop_list_by_dishes(dishes, person_count,cookbook):
+def get_shop_list_by_dishes(dishes, person_count, cookbook):
     shop_list = {}
-    for list_dishes in dishes:
-        if list_dishes in cookbook:
-            cookbook[list_dishes]
-            for dish in cookbook[list_dishes]:
+    for selected_dish in dishes:
+        if selected_dish in cookbook:
+            for dish in cookbook[selected_dish]:
                 if dish['ingridient_name'] in shop_list:
-                    shop_list[dish['ingridient_name']]['quantity'] += (int(dish['quantity'])*person_count)
+                    shop_list[dish['ingridient_name']]['quantity'] += (dish['quantity'])*person_count
                 else:
                     shop_list[dish['ingridient_name']] = {'measure': dish['measure'], 'quantity': int(dish['quantity'])*person_count}
         else:
-            print('Рецепта блюда {} нет в кулинарной книге'.format(list_dishes))
-            
+            print('Рецепта блюда {} нет в кулинарной книге'.format(selected_dish))
     print(shop_list)
 
 
 cookbook = get_cookbook()
-get_shop_list_by_dishes(['Фахитос','Омлет'], 2, cookbook)
+get_shop_list_by_dishes(['Пельмени', 'Омлет'], 2, cookbook)
